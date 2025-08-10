c0e93397478880c056a42ef0212677dcf0f90da2
Juan <702juanpablo2019@gmail.com>
2025-08-10 01:01:32 -0300

    Arruma db.column para db.Column

--- models.py
+++ models.py
@@ -4,4 +4,4 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    status = db.column(db.String(50), default="pendente")
+    status = db.Column(db.String(50), default="pendente")
