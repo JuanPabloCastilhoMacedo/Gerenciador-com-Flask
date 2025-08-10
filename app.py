8e0e96aed4a7526e975585b5a7d1a7ab059c24a8
Juan <702juanpablo2019@gmail.com>
2025-08-10 16:53:05 -0300

    Alterando o Blueprint de bp para routes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from models import db
 from config import Config
-from routes import bp
+from routes import routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
 
-app.register_blueprint(bp)
+app.register_blueprint(routes)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
