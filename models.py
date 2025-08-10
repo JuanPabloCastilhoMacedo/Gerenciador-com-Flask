83236cc238c2b606ad4c0780495e2ce363b166a9
Juan <702juanpablo2019@gmail.com>
2025-08-10 01:36:24 -0300

    Adicionando conteúdo no models.py

--- models.py
+++ models.py
@@ -1,7 +1,17 @@
 from config import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text, nullable=True)
     status = db.Column(db.String(50), default="pendente")
+
+class User(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha_hash = db.Column(db.String(128), nullable=False)
